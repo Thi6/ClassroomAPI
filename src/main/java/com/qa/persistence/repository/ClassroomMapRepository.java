0dd881243009859e1d2e6ed2782c91de08b1e079
Thi6 <thi.dnguyen96@gmail.com>
2019-06-07 14:38:38 +0100

    developed rest layer for read

--- src/main/java/com/qa/persistence/repository/ClassroomMapRepository.java
+++ src/main/java/com/qa/persistence/repository/ClassroomMapRepository.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
 
 import com.qa.persistence.domain.Classroom;
 import com.qa.util.JSONUtil;
@@ -13,8 +14,8 @@ import com.qa.util.JSONUtil;
 public class ClassroomMapRepository implements ClassroomRepository {
 
 	private Map<Integer, Classroom> classroomMap = new HashMap<>();
-
-	private JSONUtil util = new JSONUtil();
+	@Inject
+	private JSONUtil util;
 
 	public String getAllClassrooms() {
 		return util.getJSONForObject(classroomMap);
